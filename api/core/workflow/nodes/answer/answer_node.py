8f1e2d5960a3000fcff8f8d5c49bb3a2d412c70a
sxzdy <guomou0016@163.com>
2025-01-08 05:56:37 +0000

    answer修改

--- api/core/workflow/nodes/answer/answer_node.py
+++ api/core/workflow/nodes/answer/answer_node.py
@@ -1,6 +1,8 @@
 from collections.abc import Mapping, Sequence
 from typing import Any, cast
 
+from oci.data_flow.models import NodeCount
+
 from core.variables import ArrayFileSegment, FileSegment
 from core.workflow.entities.node_entities import NodeRunResult
 from core.workflow.nodes.answer.answer_stream_generate_router import AnswerStreamGeneratorRouter
@@ -16,6 +18,7 @@ from core.workflow.utils.variable_template_parser import VariableTemplateParser
 from models.workflow import WorkflowNodeExecutionStatus
 
 
+
 class AnswerNode(BaseNode[AnswerNodeData]):
     _node_data_cls = AnswerNodeData
     _node_type: NodeType = NodeType.ANSWER
@@ -27,25 +30,36 @@ class AnswerNode(BaseNode[AnswerNodeData]):
         """
         # generate routes
         generate_routes = AnswerStreamGeneratorRouter.extract_generate_route_from_node_data(self.node_data)
-
+        print('0000',generate_routes)
         answer = ""
+        answer1 = ""
         files = []
         for part in generate_routes:
             if part.type == GenerateRouteChunk.ChunkType.VAR:
                 part = cast(VarGenerateRouteChunk, part)
                 value_selector = part.value_selector
-                variable = self.graph_runtime_state.variable_pool.get(value_selector)
-                if variable:
-                    if isinstance(variable, FileSegment):
-                        files.append(variable.value)
-                    elif isinstance(variable, ArrayFileSegment):
-                        files.extend(variable.value)
-                    answer += variable.markdown
+                if value_selector[1] == 'result':
+                    variable_1 = self.graph_runtime_state.variable_pool.get(value_selector)
+                    if variable_1:
+                        if isinstance(variable_1, FileSegment):
+                            files.append(variable_1.value)
+                        elif isinstance(variable_1, ArrayFileSegment):
+                            files.extend(variable_1.value)
+                        answer1 += variable_1.markdown
+                else:
+                    variable = self.graph_runtime_state.variable_pool.get(value_selector)
+                    if variable:
+                        if isinstance(variable, FileSegment):
+                            files.append(variable.value)
+                        elif isinstance(variable, ArrayFileSegment):
+                            files.extend(variable.value)
+                        answer += variable.markdown
+
             else:
                 part = cast(TextGenerateRouteChunk, part)
                 answer += part.text
 
-        return NodeRunResult(status=WorkflowNodeExecutionStatus.SUCCEEDED, outputs={"answer": answer, "files": files})
+        return NodeRunResult(status=WorkflowNodeExecutionStatus.SUCCEEDED, outputs={"answer": answer, "files": files,'code': answer1})
 
     @classmethod
     def _extract_variable_selector_to_variable_mapping(
